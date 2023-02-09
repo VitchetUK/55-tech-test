2c6b6845d9ffab0ff83fd5055ce24bdef42a9474
Vitchet Uk <vitchet.uk@hotmail.com>
2023-02-09 22:51:07 +0100

    correction made

--- technical-test-55.js
+++ technical-test-55.js
@@ -1,68 +1,30 @@
-class Shoes {
-  constructor(name, price, id) {
-    this.name = name;
-    this.price = price;
+// Clone relevant infos into arrays
+const liItems = [...document.querySelectorAll("li.l-productgrid__item")].filter(
+  (elem) => !elem.matches(".l-productgrid__item--editorial")
+);
+const itemsRects = [...document.querySelectorAll(".c-product__item")].map(
+  (el) => el.getBoundingClientRect()
+);
+const itemsName = [...document.querySelectorAll(".c-product__name")].map(
+  (el) => el.innerHTML
+);
+const itemsPrice = [
+  ...document.querySelectorAll(".c-price__value--current"),
+].map((el) => el.innerHTML);
 
-    // this.id = id;
-  }
-
-  // Execute a console log of the current shoe.
-  displayInfo() {
-    console.log(`Name:${this.name}Price:${this.price}Id:${this.id}`);
-  }
-}
-
+// Function that loop through every items and console log if the if condition is met
 function getInfo() {
-  // Get all the loaded items while excluding the banner image listed as a li.
-  const liItems = [
-    ...document.querySelectorAll("li.l-productgrid__item"),
-  ].filter((elem) => !elem.matches(".l-productgrid__item--editorial"));
-
-  const divItem = document.querySelector(".l-productgrid__wrapper");
-
-  // Get all the newly loaded items not included in the initial load.
-  const liDataAction = [
-    ...divItem.querySelectorAll('[data-action="loadMoreProducts"]'),
-  ].filter((elem) => !elem.matches(".l-productgrid__item--editorial"));
-  //const divEdito = document.querySelector(".l-productgrid__item--editorial")
-
-  //console.log(liDataAction.length);
-
-  // Loop over every items to retrieve the information we're looking for. Execute displayInfo before going over the next item.
   for (let i = 0; i < liItems.length; i++) {
-    const itemName = document.getElementsByClassName("c-product__name");
-    const itemPrice = document.getElementsByClassName(
-      "c-price__value--current"
-    );
-
-    // WIP: Tried to retrieve the unique PID of each item.
-    //const itemIdDiv = document.querySelector(".c-product");
-    //const itemIdDataPid = itemIdDiv.dataset.pid;
-
-    let product = new Shoes(
-      itemName[i].textContent,
-      itemPrice[i].textContent
-      // itemIdDataPid
-    );
-    product.displayInfo();
-  }
-}
-
-// Execute getInfo() to retrieve name and price of every items.
-getInfo();
-
-// Observer used to re-execute getInfo() everytime new items are loaded.
-const observerUpdt = document.querySelector(".l-productgrid__inner");
-const config = { childList: true };
-const callback = (mutationList, observer) => {
-  for (const mutation of mutationList) {
-    if (mutation.type === "childList") {
-      getInfo();
+    const oneRectShoe = itemsRects[i];
+    const oneItemName = itemsName[i].trim();
+    const oneItemPrice = itemsPrice[i].trim();
+    if (oneRectShoe.top <= oneRectShoe.height * 1.25) {
+      let result = `Name:${oneItemName} Price:${oneItemPrice}`;
+      console.log(result);
     } else {
       return;
     }
   }
-};
+}
 
-const observer = new MutationObserver(callback);
-observer.observe(observerUpdt, config);
+getInfo();
